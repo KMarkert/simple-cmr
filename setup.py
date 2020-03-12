34662aa31cf7d16f6acc5416e06b5b02f400e7af
kmarkert <kel.markert@gmail.com>
2020-03-12 14:56:48 -0500

    adding python package setup script

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='simplecmr',
+      version='0.0.1',
+      description='Simple CMR',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='http://github.com/kmarkert/simple-cmr',
+      packages=setuptools.find_packages(),
+      author='Kel Markert',
+      author_email='kel.markert@gmail.com',
+      license='MIT',
+      zip_safe=False,
+      include_package_data=False,
+      #entry_points={
+      #  'console_scripts': [
+      #      'simplecmr = simplecmr.cli:main',
+      #  ],
+      #},
+)
